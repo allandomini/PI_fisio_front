4693178d0bb01c5507058edfb8c9e6eaf3736bd8
pemaismais <pedrodelafacul@gmail.com>
2024-09-13 15:44:22 -0300

    basic routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/layout/home/home.component';
+import { LoginComponent } from './components/layout/login/login.component';
+import { FormComponent } from './components/layout/form/form.component';
+import { ResultComponent } from './components/layout/result/result.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: HomeComponent},
+    {path: "login", component: LoginComponent},
+    {path: "form", component: FormComponent },
+    {path: "result", component: ResultComponent }
+];
