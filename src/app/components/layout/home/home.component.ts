83426b415a6bb689d09937d6e00740393f3aa3d8
AllanDomini <allanmarcosgarcia@gmail.com>
2024-10-08 14:53:14 -0300

    final home

--- src/app/components/layout/home/home.component.ts
+++ src/app/components/layout/home/home.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  constructor(private router: Router) {}
 
+  navigateToForm() {
+    this.router.navigate(['/form']);
+  }
 }
