4df44eaea2367ecb441da391b8c5b201d82eb50c
pemaismais <pedrodelafacul@gmail.com>
2024-10-16 21:31:48 -0300

    impl: pagination for users CRUD

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -3,6 +3,7 @@ import { inject, Injectable } from '@angular/core';
 import { environment } from '../environments/enviroment';
 import { User } from '../models/user';
 import { Observable } from 'rxjs';
+import { UserPage } from '../models/user-page';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +12,9 @@ export class UserService {
   http = inject(HttpClient);
   api = `${environment.BACKEND_URL}/api/user`;
 
-  findAll(): Observable<User[]>{
-    return this.http.get<User[]>(this.api);
+  findAll(page = 0, size = 10 ): Observable<UserPage>{
+    return this.http.get<UserPage>(this.api , {params:{ page, size}});
   }
+  
   constructor() { }
 }
