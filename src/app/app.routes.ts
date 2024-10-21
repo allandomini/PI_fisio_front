1890bb81b34e79d94bdedce81c27d1b442c4867f
juliane-dev0-0 <juhjuju030@gmail.com>
2024-10-21 20:56:54 -0300

    login social e login principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,31 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/layout/home/home.component';
-import { LoginComponent } from './components/layout/login/login.component';
+import { LoginComponent } from '../app/components/login/login.component';
 import { FormComponent } from './components/layout/form/form.component';
 import { ResultComponent } from './components/layout/result/result.component';
 import { AdminComponent } from './components/layout/admin/admin.component';
 import { AdminGuard } from './guard/admin.guard';
 import { ExerciseManagementComponent } from './components/exercise/exercisemanagement/exercisemanagement.component';
 import { UserManagementComponent } from './components/user/usermanagement/usermanagement.component';
+import { LoginGoogleComponent } from '../app/components/login-google/login-google.component';
+import { MainComponent } from '../app/components/main/main.component';
+import { HeaderComponent } from './components/header/header.component';
+
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'form', component: FormComponent },
-  { path: 'result', component: ResultComponent },
+  { path: '', component: MainComponent,
+    children:[
+    {path: 'home', component: HomeComponent},
+    {path: 'google', component: LoginGoogleComponent},
+    { path: 'login', component: LoginComponent },
+    { path: 'form', component: FormComponent },
+    { path: 'result', component: ResultComponent },
+  ] },
   {path: 'admin', component: AdminComponent, canActivate: [AdminGuard],
     children:[
       {path: 'exercise', component: ExerciseManagementComponent},
       {path: 'user', component: UserManagementComponent}
     ]
   },
-  {path: '**', redirectTo: ''}
+
 ];
---
+++ src/app/components/main/main.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../../components/header/header.component";
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { LoginGoogleComponent } from '../login-google/login-google.component';
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [HeaderComponent, RouterOutlet, FooterComponent, LoginGoogleComponent],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss'
+})
+export class MainComponent {
+
+}
