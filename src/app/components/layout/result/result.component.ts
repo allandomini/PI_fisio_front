7b59822122d9e77af58fdf0323c98ab4e1fd9fa0
juliane-dev0-0 <juhjuju030@gmail.com>
2024-10-28 15:45:55 -0300

    tela de resultados

--- src/app/components/layout/result/result.component.ts
+++ src/app/components/layout/result/result.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-
+import {MatTabsModule} from '@angular/material/tabs';
 @Component({
   selector: 'app-result',
   standalone: true,
-  imports: [],
+  imports: [MatTabsModule],
   templateUrl: './result.component.html',
   styleUrl: './result.component.scss'
 })
+
+
 export class ResultComponent {
 
 }
